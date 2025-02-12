5f88f35c55aee74f1ac5bc8f5464f2a731353d93
sikancpm <gomsjefferson217@gmail.com>
2025-02-12 16:56:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
     const optionButtons = document.querySelectorAll(".option-button");
+    const loginButton = document.getElementById("loginButton");
+    const loginModal = document.getElementById("loginModal");
+    const closeLogin = document.getElementById("closeLogin");
 
     optionButtons.forEach(button => {
         button.addEventListener("click", () => {
             const creator = button.getAttribute("data-creator");
             window.location.href = `${creator}.html`;
         });
+    });
+
+    // Funcionalidad del botón de Iniciar Sesión
+    loginButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        loginModal.style.display = "flex";
+    });
+
+    closeLogin.addEventListener("click", () => {
+        loginModal.style.display = "none";
+    });
+
+    window.addEventListener("click", (event) => {
+        if (event.target === loginModal) {
+            loginModal.style.display = "none";
+        }
     });
 });
